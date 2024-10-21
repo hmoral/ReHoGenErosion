94a8d6afa667b0effbe730b81738408ff05dcff5
Hernan E. Morales <hern.moral@gmail.com>
2024-10-21 08:41:35 +0200

    Add files via upload

---
+++ SDM/readme.mk
@@ -0,0 +1,19 @@
+The following scripts include the steps followed to perform a species distribution model for the endangered Australian bird species *Anthochaera phrygia* following a multi-temporal calibration approach (Nogués-Bravo, 2009; Maiorano et al., 2012; Teresa et al., 2013). The modeling was performed using the Biomod2 package in R.
+
+## Prepare input data for the model:
+- 1_INPUT_ENV_DATA
+- 2_INPUT_SPECIES_DATA
+- 3_INPUT_DATAFRAME
+
+## Generate the model:
+- 4_MODELLING
+
+## Average model outputs:
+- 5_AVERAGING_MAPS
+
+## Perform a Multivariate Environmental Similarity Surface (MESS) analysis:
+- 6_MESS_EXTRAPOLATION_ANALYSIS
+
+Each `.R` file contains the list of requirements for each step (libraries). The R version used for the project was 4.2.0.
+
+Contacts: Ester Milesi (estermilesi96@gmail.com).
